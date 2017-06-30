0c194ce4082988a62100a34a3a1d2b595fb86048
Casey Nord <nordcw@gmail.com>
2017-06-30 15:59:42 -0700

    Added Intro to Visual Studio

---
+++ Introduction to Visual Studio/Nord.SimpleConsoleApp/Nord.SimpleConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nord.SimpleConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a comment
+            string greeting = "Hello World";
+
+            string input = "Casey Nord";
+
+            Console.Write(greeting + ' ');
+            Console.WriteLine(SplitName(input)[0]);
+        }
+
+        static string[] SplitName(string input)
+        {
+            return input.Split(' ');
+        }
+    }
+}
