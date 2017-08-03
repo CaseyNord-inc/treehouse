2b7ce31137a5c88369bc10282651c69fa2541ae9
Casey Nord <nordcw@gmail.com>
2017-08-02 21:39:04 -0700

    Added C# Collections coursework

---
+++ C# Collections/Collections/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main()
+        {
+            HashSet<Student> students = new HashSet<Student>
+            {
+                new Student() { Name = "Sally", GradeLevel = 3 },
+                new Student() { Name = "Bob", GradeLevel = 3 },
+                new Student() { Name = "Sally", GradeLevel = 2 },
+            };
+
+            Student joe = new Student() { Name = "Joe", GradeLevel = 2 };
+
+            students.Add(joe);
+
+            Student duplicateJoe = new Student() { Name = "Joe", GradeLevel = 2 };
+
+            students.Add(duplicateJoe);
+
+            if(students.Contains(joe))
+            {
+                do something
+            }
+
+            foreach (Student student in students)
+            {
+                Console.WriteLine($"{student.Name} is in grade {student.GradeLevel}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
