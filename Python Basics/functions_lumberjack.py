0f3f17e63c69bf72ab3e596f5b2e692958ea7868
Casey Nord <nordcw@gmail.com>
2017-08-02 21:45:47 -0700

    Added Python Basics

---
+++ Python Basics/functions_lumberjack.py
@@ -0,0 +1,19 @@
+def hows_the_parrot():
+    print("He's pining for the fjords!")
+
+hows_the_parrot()
+
+def lumberjack(name):
+    if name.lower() == 'casey':
+        print("Casey's a lumberjack and he's OK!")
+    else:
+        print("{} sleeps all night and {} works all day!".format(name, name))
+
+lumberjack("Casey")
+
+def lumberjack2(name, pronoun):
+        print("{}'s a lumberjack and {}'s OK!".format(name, pronoun))
+        print("{} sleeps all night and {} works all day!".format(pronoun, pronoun))
+
+lumberjack2("Casey", "he")
+lumberjack2("Kira", "she")
